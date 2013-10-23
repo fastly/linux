cf90d47ff261e88e42b3e9914a7a1fd9999c1444
Artur Bergman <artur@fastly.com>
2013-10-23 06:54:28 +0000

    pass syncookie down and dont do multipath for them

--- net/ipv4/ping.c
+++ net/ipv4/ping.c
@@ -792,7 +792,7 @@ static int ping_v4_sendmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *m
 			   inet_sk_flowi_flags(sk), faddr, saddr, 0, 0);
 
 	security_sk_classify_flow(sk, flowi4_to_flowi(&fl4));
-	rt = ip_route_output_flow(net, &fl4, sk);
+	rt = ip_route_output_flow(net, &fl4, sk, 0);
 	if (IS_ERR(rt)) {
 		err = PTR_ERR(rt);
 		rt = NULL;
