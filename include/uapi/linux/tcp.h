b344f322db6841ff95ae617582d8bb9c7c9905ee
dormando <dormando@rydia.net>
2014-06-28 17:37:34 -0700

    accept a second number for TCP_CWND
    pivoting to 99 to make future maintenance simpler.

--- include/uapi/linux/tcp.h
+++ include/uapi/linux/tcp.h
@@ -113,6 +113,7 @@ enum {
 #define TCP_TIMESTAMP		24
 #define TCP_NOTSENT_LOWAT	25	/* limit number of unsent bytes in write queue */
 #define TCP_CWND            19 /* Set congestion window */
+#define TCP_CWND2           99 /* Set congestion window */
 
 struct tcp_repair_opt {
 	__u32	opt_code;
